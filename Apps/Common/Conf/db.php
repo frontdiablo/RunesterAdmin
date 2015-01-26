ed4d2895617e6e088d7a729ffa2462c35a6b0b6d
frontdiablo <front_diablo@163.com>
2015-01-26 18:05:03 +0800

    Signed-off-by: frontdiablo <front_diablo@163.com>

--- Apps/Common/Conf/db.php
+++ Apps/Common/Conf/db.php
@@ -3,9 +3,9 @@ return array(
 	//数据库配置
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'myresume',  //如果数据库名都相同的话，你可以不用定义多个
+	'DB_NAME'=>'rune',
 	'DB_USER'=>'root',
-	'DB_PWD'=>'3C8FtB6lCvRx',
+	'DB_PWD'=>'123456',
 	'DB_PORT'=>'3306',
 	'DB_PREFIX'=>'ra_',
 );
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RunesterAdmin
 RunesterAdmin是基于ThinkPHP的网站管理系统。
+
+使用方法：
+在本地数据库中运行sql.sql文件后，
+修改 /Apps/Common/Conf/db.php 中的数据库配置信息即可。
